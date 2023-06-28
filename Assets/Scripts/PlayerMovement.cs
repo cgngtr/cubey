eb144bfe639fcf0bcb652ab78f9d168fa327ee72
Reveck <cgngtr5026@hotmail.com>
2023-06-28 18:14:10 +0300

    Wallslide & walljump were added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,87 +6,178 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float dashDistance = 5f;
-    [SerializeField] private float dashDuration = 0.2f;
-    [SerializeField] private float dashCooldown = 1f;
-    [SerializeField] private bool canDash = true;
+    private TrailRenderer tr;
+
+    #region BASIC MOVEMENT
+
     [SerializeField] private bool isGrounded = true;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask whatIsGround;
+
+    [SerializeField] private float checkRadius;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpForce = 20f;
-    [SerializeField] private float dashForce = 20f;
-    private int jumpAmount = 2;
-    private int jumpCount = 0;
+    [SerializeField] private int extraJumpsValue = 1;
+    private int extraJumps;
+    private float input;
+    private bool isFacingRight = true;
 
+    #endregion
+
+    #region DASH SETTINGS
+    private bool canDash = true;
+    private bool isDashing;
+    [SerializeField] private float dashingPower = 5f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+    #endregion
+
+    #region OTHER
+
+    private int invokeCount;
+
+    #endregion
+
+    #region WALLSLIDE & JUMP
+
+    private bool isTouchingWall;
+    private bool isWallSliding;
+    [SerializeField] private LayerMask whatIsWall;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallSlidingSpeed;
+    private bool wallJumping;
+    [SerializeField] private float xWallForce;
+    [SerializeField] private float yWallForce;
+    [SerializeField] private float wallJumpTime;
+
+
+    #endregion
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<TrailRenderer>();
+        //InvokeRepeating("InvokeFunction", 0f, 0.5f);
     }
 
 
     private void Update()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-
-        Vector2 movement = new Vector2(moveHorizontal, 0f);
-        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-
+        if(isDashing)
         {
-            Jump();
+            return;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        input = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
 
+        if(input > 0 && !isFacingRight)
         {
-            Dash();
+            Flip();
+        }
+        else if(input < 0 && isFacingRight) 
+        {
+            Flip();
+        }
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        if(isGrounded)
+        {
+            extraJumps = extraJumpsValue;  
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) 
+        {
+            StartCoroutine(Dash());
+        }
+
+        isTouchingWall = Physics2D.OverlapCircle(wallCheck.position, checkRadius, whatIsGround);
+
+        if (isTouchingWall && isGrounded && input != 0)
+        {
+            isWallSliding = true;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+            
+        if(isWallSliding)
+        {   
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isWallSliding)
+        {
+            wallJumping = true;
+            Invoke("SetWallJumpingToFalse", wallJumpTime);
+        }
+
+        if(wallJumping)
+        {
+            rb.velocity = new Vector2(xWallForce * -input,yWallForce);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
         }
     }
 
-    private void Jump()
+    private void SetWallJumpingToFalse()
     {
-        if(jumpCount < 2) 
-        {
-            Debug.Log("Performing jump!");
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            jumpCount++;
-        }
-        else if(jumpCount == 2)
-        {
-            isGrounded = false;
-        }
-        
+        wallJumping = false;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Flip()
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        isFacingRight =! isFacingRight;
+    }
+
+    private void InvokeFunction()
+    {
+        Debug.Log(rb.velocity.y);
+        invokeCount++;
+
+        if (invokeCount >= 500)
         {
-            isGrounded = true;
-            jumpCount = 0;
+            CancelInvoke("InvokeFunction");
         }
     }
 
-    private void Dash()
+    private IEnumerator Dash()
     {
-        Debug.Log("Performing dash!");
-        Vector2 dashDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
-        rb.AddForce(dashDirection * dashForce / dashDuration, ForceMode2D.Impulse);
         canDash = false;
-        Invoke(nameof(EndDash), dashDuration);
-        Invoke(nameof(EnableDash), dashCooldown);
-    }
-
-    private void EndDash()
-    {
-        //rb.velocity = Vector2.zero;
-        canDash = false;
-    }
-
-    private void EnableDash()
-    {
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        Vector2 dashDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        rb.AddForce(dashDirection * dashingPower, ForceMode2D.Impulse);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
     }
 
